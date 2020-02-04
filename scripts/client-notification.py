ff829f0d901fd2ade4c1e6860267b6533419a287
Harsh Deshpande <harshavardhan.deshpande@ipa.fraunhofer.de>
2020-02-04 13:31:47 +0100

    Added server and client for data and event notification example

---
+++ scripts/client-notification.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+import sys
+import time
+
+try:
+    from IPython import embed
+except ImportError:
+    import code
+
+    def embed():
+        vars = globals()
+        vars.update(locals())
+        shell = code.InteractiveConsole(vars)
+        shell.interact()
+
+from opcua import Client
+
+class SubHandler(object):
+
+    """
+    Subscription Handler. To receive events from server for a subscription
+    data_change and event methods are called directly from receiving thread.
+    Do not do expensive, slow or network operation there. Create another
+    thread if you need to do such a thing
+    """
+
+    def datachange_notification(self, node, val, data):
+        print("Python: New data change event", node, val)
+
+    def event_notification(self, event):
+        print("Python: New event", event)
+
+
+if __name__ == "__main__":
+
+    client = Client("opc.tcp://localhost:4840/freeopcua/server/")
+    try:
+        client.connect()
+
+        # Client has a few methods to get proxy to UA nodes that should always be in address space such as Root or Objects
+        root = client.get_root_node()
+        print("Objects node is: ", root)
+
+        # Node objects have methods to read and write node attributes as well as browse or populate address space
+        print("Children of root are: ", root.get_children())
+
+        # get a specific node knowing its node id
+        #var = client.get_node(ua.NodeId(1002, 2))
+        #var = client.get_node("ns=3;i=2002")
+        #print(var)
+        #var.get_data_value() # get value of node as a DataValue object
+        #var.get_value() # get value of node as a python builtin
+        #var.set_value(ua.Variant([23], ua.VariantType.Int64)) #set node value using explicit data type
+        #var.set_value(3.9) # set node value using implicit data type
+
+        # Now getting a variable node using its browse path
+        myvar = root.get_child(["0:Objects", "2:MyObject", "2:MyVariable"])
+        obj = root.get_child(["0:Objects", "2:MyObject"])
+
+        # Stacked myvar access
+        print("myvar is: ", root.get_children()[0].get_children()[1].get_variables()[0].get_value())
+
+        handler = SubHandler()
+        sub = client.create_subscription(500, handler)
+        handle = sub.subscribe_data_change(myvar)
+
+        sub.subscribe_events()
+        time.sleep(0.1)
+
+        embed()
+    finally:
+        client.disconnect()
